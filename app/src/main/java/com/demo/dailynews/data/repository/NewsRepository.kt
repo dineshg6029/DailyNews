e7efd8198270cd1b1bb68e0fbb2f1ef58803214d
DINESH G <gdineshg029@gmail.com>
2022-03-15 13:33:09 +0530

    Added the @ActivityRetainedScoped annotation to the NewsRepository because even if the fragment is restarted the newsrepository object is restored

--- app/src/main/java/com/demo/dailynews/data/repository/NewsRepository.kt
+++ app/src/main/java/com/demo/dailynews/data/repository/NewsRepository.kt
@@ -2,10 +2,12 @@ package com.demo.dailynews.data.repository
 
 import com.demo.dailynews.data.model.NewsApiResult
 import com.demo.dailynews.data.retrofit.NewsApiService
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
+@ActivityRetainedScoped
 class NewsRepository @Inject constructor(private val newsApiService: NewsApiService) {
 
     suspend fun getNews(apiKey:String) : Flow<NewsApiResult>{
