fa86c9a764ac9eede927c3e15cee7bd820a59b81
DINESH G <gdineshg029@gmail.com>
2022-03-16 11:52:53 +0530

    Implemented the below changes 1)Added the SwipeRefreshLayout view in news_list_fragment.xml for pull to refresh 2)Also added condition to check internet is present before making api request

--- app/src/main/java/com/demo/dailynews/ui/news/view/NewsListFragment.kt
+++ app/src/main/java/com/demo/dailynews/ui/news/view/NewsListFragment.kt
@@ -4,26 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.demo.dailynews.R
 import com.demo.dailynews.data.model.Article
 import com.demo.dailynews.databinding.NewsListFragmentBinding
 import com.demo.dailynews.ui.news.adapters.ArticlesListAdapter
 import com.demo.dailynews.ui.news.interfaces.CustomItemClickListener
 import com.demo.dailynews.ui.news.viewmodel.NewsListViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.java.predictweather.data.retrofit.NetworkUtils
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class NewsListFragment : Fragment(), CustomItemClickListener {
+class NewsListFragment : Fragment(), CustomItemClickListener, SwipeRefreshLayout.OnRefreshListener {
 
     private val viewModel: NewsListViewModel by viewModels()
     @Inject lateinit var articlesListAdapter: ArticlesListAdapter
+    @Inject lateinit var networkUtils: NetworkUtils
     private lateinit var dataBindingObject: NewsListFragmentBinding
     private var articles: MutableList<Article>? = null
 
@@ -31,22 +34,21 @@ class NewsListFragment : Fragment(), CustomItemClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dataBindingObject =
-            DataBindingUtil.inflate(inflater, R.layout.news_list_fragment, container, false)
+        DataBindingUtil.inflate<NewsListFragmentBinding>(inflater, R.layout.news_list_fragment, container, false)
+            .also { dataBindingObject = it }
         return dataBindingObject.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        dataBindingObject.swipeRefreshLayout.setOnRefreshListener(this)
         observeLiveData()
         loadRecyclerView()
     }
 
     override fun onResume() {
         super.onResume()
-        if (viewModel.articleList.value == null) {
-            viewModel.getNewsArticles()
-        }
+        if (viewModel.articleList.value == null) fetchData()
         viewModel.articleList.value?.let {
             articlesListAdapter.update(it)
         }
@@ -62,23 +64,27 @@ class NewsListFragment : Fragment(), CustomItemClickListener {
     private fun observeLiveData() {
         viewModel.loadingStatus.observe(this, {
             if (it) {
-                dataBindingObject.loadingBar.visibility = View.VISIBLE
+                dataBindingObject.swipeRefreshLayout.isRefreshing = true
                 dataBindingObject.errorTextView.visibility = View.GONE
                 dataBindingObject.articlesRecyclerView.visibility = View.GONE
             } else {
-                dataBindingObject.loadingBar.visibility = View.GONE
+                dataBindingObject.swipeRefreshLayout.isRefreshing = false
             }
         })
 
         viewModel.errorMessage.observe(
             this,
             {
-                dataBindingObject.errorTextView.visibility = View.VISIBLE
-                dataBindingObject.errorTextView.text = it
+                dataBindingObject.swipeRefreshLayout.isRefreshing = false
+                with(dataBindingObject.errorTextView){
+                    visibility = View.VISIBLE
+                    text = it
+                }
             },
         )
 
         viewModel.articleList.observe(this, {
+            dataBindingObject.swipeRefreshLayout.isRefreshing = false
             if (!it.isNullOrEmpty()) {
                 dataBindingObject.articlesRecyclerView.visibility = View.VISIBLE
                 articles = it.toMutableList()
@@ -97,6 +103,23 @@ class NewsListFragment : Fragment(), CustomItemClickListener {
                     it[position]
                 )
             findNavController().navigate(directions)
+        }
+    }
+
+    override fun onRefresh() {
+        fetchData()
+    }
+
+    private fun fetchData() {
+        if (networkUtils.isConnected()) {
+            viewModel.getNewsArticles()
+        } else {
+            Snackbar.make(
+                dataBindingObject.swipeRefreshLayout,
+                getString(R.string.internet_error),
+                Snackbar.LENGTH_LONG
+            ).show()
+            dataBindingObject.swipeRefreshLayout.isRefreshing = false
         }
     }
 
